d92ad438f6444643700f9cdb0688214b32ba673c
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-05-03 15:22:09 +0200

    Start project

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+
+    <form action="words.php" method="POST">
+        <label for="paragrafo">Paragrafo</label>
+        <input type="text" name="paragrafo" id="paragrafo">
+
+        <label for="parola">Parola</label>
+        <input type="text" name="parola" id="parola">
+
+        <button type="submit">Invia</button>
+    </form>
+    
+</body>
+</html>
